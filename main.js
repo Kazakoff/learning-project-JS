7b866a37c1dcdcaba55324c191b705b7ba5274a3
Kazakoff <wadimchik@mail.ru>
2018-11-10 15:14:34 +0300

    debug probe

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 // https://tproger.ru/translations/frontend-testing-2/
+const arrayFunctions = require('ArrayFunction')
 var arr = [1,2,3,4,5];
-console.log('foreach');
-arrayFuncModule.foreach(arr,function(a,i){ console.log('a['+i+']='+a)});
+/*console.log('foreach');
+arrayFunctions.foreach(arr,function(a,i){ console.log('a['+i+']='+a)});
 
-console.log('map');
-var arrMap = arrayFuncModule.map(arr,function(arrI, i){return arrI*i});
-arrayFuncModule.foreach(arrMap,function(a){ console.log(a)});
-
-console.log('take');
-arrMap = arrayFuncModule.take(arr,3);
-arrayFuncModule.foreach(arrMap,function(a){ console.log(a)});
-
-console.log('skip');
-arrMap = arrayFuncModule.skip(arr,3);
-arrayFuncModule.foreach(arrMap,function(a){ console.log(a)});
+arrayFunctions.forEach(resultArray,function(arrItm, i, array){array[i] += i})
+*/
+function reduce(a, callback, initialValue ){
+    var accumulate = initialValue ? initialValue : 0;
+    var length = a.length;
+    for (i = 0; i < length; i++) {
+        accumulate = callback(accumulate, a[i], i, a);
+    }
+    return accumulate;
+}
+const arr = [1,2,3];
+ var c = reduce(arr,function(faktorial, arrItm){return faktorial *= arrItm},1)

37db4a7385c1f98b1b3a0ddaa94b5c34e4908ff2
Kazakoff <wadimchik@mail.ru>
2018-11-13 20:40:44 +0300

    h

--- modules/ArrayFunction.js
+++ modules/ArrayFunction.js
@@ -1,13 +1,13 @@
 
 module.exports.forEach = function foreach (a, callback, thisForCallback) {
-  var i = 0
+  var i = 1
   var length = a.length
   for (i = 0; i < length; i++) {
     callback.call(thisForCallback, a[i], i, a)
   }
 }
 module.exports.map = function map (a, callback, thisForCallback) {
-  var i = 0
+  var i = 1
   var mapedArr = []
   for (i = 0; i < a.length; i++) {
     mapedArr.push(callback.call(thisForCallback, a[i], i, a))

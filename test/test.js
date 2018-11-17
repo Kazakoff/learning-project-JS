315fbd0b5d26063e37c422340e7323b7e0d7381a
unknown <C:\Users\administrator\AppData\Roaming\The Bat!>
2018-11-17 12:10:19 +0300

    l2

--- test/test.js
+++ test/test.js
@@ -1,63 +1,34 @@
-const assert = require("assert")
+/* eslint-disable no-return-assign */
+/* eslint-disable no-undef */
+/* eslint-disable node/no-deprecated-api */
+const assert = require('assert')
 const arrayFunctions = require('../modules/ArrayFunction')
 
- describe('forEach', function () {
-  const arr = [1,2,3];
-  var resultArray = arr.slice(0);
-  arrayFunctions.forEach(resultArray,function(arrItm, i, arr){arr[i] += i})
-it('standart', () => {
-  assert.deepEqual(resultArray, [1,3,5]);
-} );
+describe('Array functions library', function () {
+  const arr = [1, 2, 3]
+  var resultArray = arr.slice(0)
+  arrayFunctions.forEach(resultArray, function (arrItm, i, arr) { arr[i] += i })
+  it('forEach', () => {
+    assert.deepEqual(resultArray, [1, 3, 5])
+  })
 
-var resultArray2 = arr.slice(0);
-arrayFunctions.forEach(resultArray2,function(arrItm, i, arr){arr[i] += this.length})
-it('without this', () => {
-  assert.deepEqual(resultArray2, [1,3,5]);
-} );
+  it('map', () => {
+    assert.deepEqual(arrayFunctions.map(arr, function (arrItm, i) { return arrItm * i }), [0, 2, 6])
+  })
 
-var resultArray3 = [1,2,3];
-var contextArray = [1,2,3];
-arrayFunctions.forEach(resultArray3,function(arrItm, i, arr){ arr[i] += this.length}, contextArray)
-it('with this', () => {
-  assert.deepEqual(resultArray3, [4,5,6]);
-} );
+  it('take', () => {
+    assert.deepEqual(arrayFunctions.take(arr, 2), [1, 2])
+  })
 
+  it('skip', () => {
+    assert.deepEqual(arrayFunctions.skip(arr, 2), [3])
+  })
 
-} );
+  it('reduce', () => {
+    assert.deepEqual(arrayFunctions.reduce(arr, function (faktorial, arrItm) { return faktorial *= arrItm }, 1), 6)
+  })
 
-describe('map', function () {
-    const arr = [1,2,3];
-  it('standart', () => {
-    assert.deepEqual(arrayFunctions.map(arr,function(arrItm, i){return arrItm*i}), [0,2,6]);
-  } );
- } );
-
- describe('take', function () {
-  const arr = [1,2,3];
-it('standart', () => {
-  assert.deepEqual(arrayFunctions.take(arr,2), [1,2]);
-} );
-} ); 
-
-describe('skip', function () {
-  const arr = [1,2,3,4];
-it('standart', () => {
-  assert.deepEqual(arrayFunctions.skip(arr,2), [3,4]);
-} );
-} ); 
-
- describe('reduce', function () {
-  const arr = [1,2,3];
-it('standart', () => {
-   assert.deepEqual(arrayFunctions.reduce(arr,function(faktorial, arrItm){return faktorial *= arrItm},1), 6);
-} );
-} );
-
- describe('filter', function () {
-  const arr = [1,2,3,4,5,6];
-it('standart', () => {
-  assert.deepEqual(arrayFunctions.filter(arr,function(arrItm, i){return arrItm % 2 == 0}), [2,4,6]);
-} );
-} );
-
-
+  it('filter', () => {
+    assert.deepEqual(arrayFunctions.filter(arr, function (arrItm, i) { return arrItm % 2 === 0 }), [2])
+  })
+})

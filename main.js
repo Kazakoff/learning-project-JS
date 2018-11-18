fb8105eb9148e9651bc939fe5782356c47106063
Kazakoff <wadimchik@mail.ru>
2018-11-18 10:15:41 +0300

    Array Object

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 // https://tproger.ru/translations/frontend-testing-2/
 var arr = [1,2,3,4,5];
-console.log('foreach');
-arrayFuncModule.foreach(arr,function(a,i){ console.log('a['+i+']='+a)});
 
-console.log('map');
-var arrMap = arrayFuncModule.map(arr,function(arrI, i){return arrI*i});
-arrayFuncModule.foreach(arrMap,function(a){ console.log(a)});
+const arrayObjectModule = require('./modules/ArrayObject')
 
-console.log('take');
-arrMap = arrayFuncModule.take(arr,3);
-arrayFuncModule.foreach(arrMap,function(a){ console.log(a)});
 
-console.log('skip');
-arrMap = arrayFuncModule.skip(arr,3);
-arrayFuncModule.foreach(arrMap,function(a){ console.log(a)});

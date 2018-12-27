a6639581c0d513841313daf75afcc461f821e39d
Kazakoff <wadimchik@mail.ru>
2018-12-27 21:52:45 +0300

    babel

---
+++ modules-dist/ArrayLib.js
@@ -0,0 +1,87 @@
+module.exports = function () {
+  function chain(initArray) {
+    var arr = initArray.slice(0);
+    function libCall(f) {
+      return function () {
+        var fb = f.bind(this, arr);
+        arr = fb.apply(this, arguments);
+        return this;
+      };
+    }
+    return {
+      take: libCall(take),
+      skip: libCall(skip),
+      foreach: libCall(foreach),
+      map: libCall(map),
+      value: function () {
+        return arr;
+      }
+    };
+  }
+
+  var sum = (() => {
+    var cache = {};
+    return function (array, start, end) {
+      let cacheId = array + '-' + start + ',' + end;
+      if (cacheId in cache) {
+        return cache[cacheId];
+      } else {
+        let s = 0;
+        for (let i = start; i <= end; i++) {
+          s += array[i];
+        }
+        cache[cacheId] = s;
+        return s;
+      }
+    };
+  })();
+
+  function foreach(a, callback) {
+    for (let i = 0; i < a.length; i++) {
+      callback(a[i], i, a);
+    }
+  }
+
+  function map(a, callback) {
+    var mapedArr = [];
+    for (let i = 0; i < a.length; i++) {
+      mapedArr.push(callback(a[i], i, a));
+    }
+    return mapedArr;
+  }
+
+  function take(a, count) {
+    return a.slice(0, count);
+  }
+
+  function skip(a, count) {
+    return a.slice(count);
+  }
+
+  function reduce(a, callback, initialValue) {
+    var accumulate = initialValue || 0;
+    for (var i = 0; i < a.length; i++) {
+      accumulate = callback(accumulate, a[i], i, a);
+    }
+    return accumulate;
+  }
+
+  function filter(a, callback) {
+    var filteredArr = [];
+    for (let i = 0; i < a.length; i++) {
+      if (callback(a[i], i, a)) filteredArr.push(a[i]);
+    }
+    return filteredArr;
+  }
+
+  return {
+    sum: sum,
+    chain: chain,
+    foreach: foreach,
+    map: map,
+    take: take,
+    skip: skip,
+    reduce: reduce,
+    filter: filter
+  };
+}();
---
+++ modules-dist/ArrayLibES6.js
@@ -0,0 +1,88 @@
+class chain {
+  constructor(initArray) {
+    this.arr = initArray.slice(0);
+  }
+  libCall(f) {
+    return function () {
+      var fb = f.bind(this, this.arr);
+      this.arr = fb.apply(this, arguments);
+      return this;
+    };
+  }
+  take() {
+    this.libCall(take);
+  }
+  skip() {
+    this.libCall(skip);
+  }
+  foreach() {
+    this.libCall(foreach);
+  }
+  map() {
+    this.libCall(map);
+  }
+  value() {
+    return this.arr;
+  }
+}
+function sum(array, start, end) {
+  var cache = {};
+  let cacheId = array + '-' + start + ',' + end;
+  if (cacheId in cache) {
+    return cache[cacheId];
+  } else {
+    let s = 0;
+    for (let i = start; i <= end; i++) {
+      s += array[i];
+    }
+    cache[cacheId] = s;
+    return s;
+  }
+}
+
+function foreach(a, callback) {
+  for (let i = 0; i < a.length; i++) {
+    callback(a[i], i, a);
+  }
+}
+
+function map(a, callback) {
+  var mapedArr = [];
+  for (let i = 0; i < a.length; i++) {
+    mapedArr.push(callback(a[i], i, a));
+  }
+  return mapedArr;
+}
+
+function take(a, count) {
+  return a.slice(0, count);
+}
+
+function skip(a, count) {
+  return a.slice(count);
+}
+
+function reduce(a, callback, initialValue) {
+  var accumulate = initialValue || 0;
+  for (var i = 0; i < a.length; i++) {
+    accumulate = callback(accumulate, a[i], i, a);
+  }
+  return accumulate;
+}
+
+function filter(a, callback) {
+  var filteredArr = [];
+  for (let i = 0; i < a.length; i++) {
+    if (callback(a[i], i, a)) filteredArr.push(a[i]);
+  }
+  return filteredArr;
+}
+
+module.exports.chain = chain;
+module.exports.filter = filter;
+module.exports.reduce = reduce;
+module.exports.skip = skip;
+module.exports.take = take;
+module.exports.map = map;
+module.exports.foreach = foreach;
+module.exports.sum = sum;
